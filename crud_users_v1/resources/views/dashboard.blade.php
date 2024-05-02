5eb7ac4634409aa107ff972688d7a33488a08b65
songitcode <dung22022017cm@gmail.com>
2024-05-02 13:18:40 +0700

    update

--- crud_users_v1/resources/views/dashboard.blade.php
+++ crud_users_v1/resources/views/dashboard.blade.php
@@ -4,8 +4,6 @@
 <head>
     <title>Home</title>
     <link rel="stylesheet" href="{{ asset('/front-end/style.css') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
@@ -29,10 +27,6 @@
                         </li>
                     @endguest
                 </ul>
-                <form class="d-flex" method="GET">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
             </div>
         </div>
     </nav>
